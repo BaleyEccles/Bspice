14a41c0018d199014e44c976ad450bae7a0b233d
Baley Eccles <baleycod@gmail.com>
2025-01-25 17:50:42 +1100

    Add support for plotting in the circuit file
    Now the plot{VARIBLE} creates the relevent plot in a octave file.

--- src/main.cpp
+++ src/main.cpp
@@ -99,8 +99,83 @@ Circuit createCircuitFromTokens(std::vector<std::shared_ptr<fileParser::token>> 
   return circuit;
 }
 
+class postProcess {
+public:
+  postProcess(const std::string& octaveFileName, std::vector<double> &time, std::vector<matrix<double>> &data, matrix<symbol> &syms, std::vector<std::shared_ptr<fileParser::token>> &tokens)
+    : fileName(octaveFileName), time(time), data(data), syms(syms), tokens(tokens) {
+
+    file.open(fileName);
+
+    if (!file.is_open()) {
+      std::cerr << "Error opening file" << std::endl;
+      return;
+    }
+    createOctavePlotFileFromTokens();
+    file.close();
+  };
+  ~postProcess() {
+  }
+private:
+  const std::string fileName;
+  std::vector<double> time;
+  std::vector<matrix<double>> data;
+  matrix<symbol> syms;
+  std::vector<std::shared_ptr<fileParser::token>> tokens;
+  std::ofstream file;
+  
+  void addOctaveVarible(const std::string &name, std::vector<double> &plotData) {
+    file << (name + " = [");
+    for (int i = 0; i < plotData.size(); i++) {
+      file << std::scientific << std::setprecision(5) << plotData[i] << " ";
+    }
+    file << ("];\n");
+  };
+
+  void addPlot(const std::string &name, std::vector<double> &plotData) {
+    //std::cout << name << " " << plotData.size() << std::endl;
+    addOctaveVarible(name, plotData);
+    file << ("figure();\n");
+    file << ("plot(t, " + name + ");\n");
+    file << ("xlabel(\"t\");\n");
+    file << ("ylabel(\"" + name + "\");\n");
+  };
+
+  void createOctavePlotFileFromTokens() {
+    file << "graphics_toolkit(\"gnuplot\")\n";
+    file << "set(0, 'DefaultTextFontSize', 25);";
+    file << "set(0, 'DefaultAxesFontSize', 25);";
+    addOctaveVarible("t", time);
+  
+    for (auto& token : tokens) {
+      if (token->type == fileParser::token::PLOT) {
+        auto plotToken = dynamic_cast<fileParser::plotToken *>(token.get());
+        bool isValidPlot = false;
+        for (int row = 0; row < syms.rows; row++) {
+          if (syms.data[row][0].name == plotToken->name) {
+            std::vector<double> plotData;
+            for (auto& d : data) {
+              plotData.push_back(d.data[row][0]);
+            }
+            addPlot(plotToken->name, plotData);
+            isValidPlot = true;
+          }
+        }
+        if (!isValidPlot) {
+          std::cerr << "ERROR: `" << plotToken->name << "` was not able to be plotted." << std::endl;
+        }
+
+      }
+    }
+    file << ("pause;");
+
+  };
+
+};
+
+
+
 int main() {
-  std::vector<std::shared_ptr<fileParser::token>> tokens = fileParser::parseFile("Examples/inductor.circuit");
+  std::vector<std::shared_ptr<fileParser::token>> tokens = fileParser::parseFile("Examples/inductorCapacitor.circuit");
   Circuit circuit = createCircuitFromTokens(tokens);
   circuit.calculate();
   auto initalValues = circuit.initalValues;
@@ -112,7 +187,9 @@ int main() {
   double timeStep = 0.001;
   double endTime = 1.0;
   auto output = DAESolve(A, E, f, initalValues, timeStep, endTime);
-  createOctavePlotFile(output.first, output.second, s);
+
+  postProcess("plotData.m", output.first, output.second, s, tokens);
+  //createOctavePlotFile(output.first, output.second, s);
   
   if (false) {
     Node *Node1 = new Node("e1");
