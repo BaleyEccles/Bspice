6470ace35e6a57b39a17be28083ede04f77b30a6
Baley Eccles <baleycod@gmail.com>
2025-01-26 19:10:31 +1100

    Remove createOctavePlotFile function
    The circuit files contain the data that should be plotted, so there is
    no need to have this anymore.

--- src/main.cpp
+++ src/main.cpp
@@ -10,55 +10,6 @@
 #include <string>
 #include <vector>
 
-void createOctavePlotFile(std::vector<double> &time,
-                          std::vector<matrix<double>> &data,
-                          matrix<symbol> &names) {
-  if (data.size() != time.size()) {
-    std::cerr << "Time and data have different sizes" << std::endl;
-  }
-
-  std::string filename = "plotData.m";
-  std::ofstream file(filename);
-  if (!file.is_open()) {
-    std::cerr << "Error opening file" << std::endl;
-    return;
-  }
-  file << "graphics_toolkit(\"gnuplot\")\n";
-  file << "set(0, 'DefaultTextFontSize', 25);";
-  file << "set(0, 'DefaultAxesFontSize', 25);";
-  file << ("t = [");
-  for (int i = 0; i < data.size(); i++) {
-    file << std::scientific << std::setprecision(5) << time[i] << " ";
-  }
-  for (int j = 0; j < names.rows; j++) {
-    file << ("];\n" + names.data[j][0].name + " = [");
-    for (int i = 0; i < data.size(); i++) {
-      file << std::scientific << std::setprecision(5) << data[i].data[j][0]
-           << " ";
-    }
-  }
-  file << ("];\n");
-
-  for (int j = 0; j < names.rows; j++) {
-    file << ("figure(" + std::to_string(j + 1) + ");\n");
-    file << ("plot(t, " + names.data[j][0].name + ");\n");
-    file << ("xlabel(\"t\");\n");
-    file << ("ylabel(\"" + names.data[j][0].name + "\");\n");
-  }
-
-  /// Print voltage over component
-  // file << ("figure(" + std::to_string(names.rows + 1) + ");\n");
-  // file << ("plot(t, e2 - e3)\n");
-  // file << ("xlabel \"t\");");
-  // file << ("ylabel (\"e2-e3\");");
-
-  file << ("pause;");
-  file.close();
-};
-
-
-
-
 int main() {
   std::vector<std::shared_ptr<fileParser::token>> tokens = fileParser::parseFile("../Examples/inductorCapacitor.circuit");
   Circuit circuit = createCircuitFromTokens(tokens);
